378dbabb3dc9dba8f0749cceb31e808aa2fdf18c
luan-amorim01 <luanamorim.job@gmail.com>
2021-10-31 15:19:42 -0300

    Adding data to the main program

--- Grafo/src/aplication/Program.java
+++ Grafo/src/aplication/Program.java
@@ -1,10 +1,24 @@
 package aplication;
 
+import entities.Grafo;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Grafo<String> grafo = new Grafo<String>();
+		
+		grafo.adicionarVertice("Luan");
+		grafo.adicionarVertice("Iago");
+		grafo.adicionarVertice("Iuri");
+		grafo.adicionarVertice("Cicero");
+		grafo.adicionarVertice("Diva");
+		
+		grafo.adicionarAresta(2.0, "Luan", "Iago");
+		grafo.adicionarAresta(3.0, "Iago", "Cicero");
+		grafo.adicionarAresta(1.0, "Cicero", "Iuri");
+		grafo.adicionarAresta(1.0, "Luan", "Diva");
+		grafo.adicionarAresta(2.0, "Diva", "Iuri");
+		grafo.adicionarAresta(3.0, "Diva", "Luan");
 	}
 
 }
