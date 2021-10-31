608d66ef1d944f5235b4754053b0e161e5c3a043
luan-amorim01 <luanamorim.job@gmail.com>
2021-10-31 14:55:30 -0300

    changes in the classes aresta and vertice. And bug fix.

--- Grafo/src/entities/Aresta.java
+++ Grafo/src/entities/Aresta.java
@@ -1,5 +1,38 @@
 package entities;
 
-public class Aresta {
-
+public class Aresta<TIPO> {
+	// nível de amizade
+	private Double peso;
+	// relações de começo
+	private Vertice<TIPO> inicio;
+	// relação final
+	private Vertice<TIPO> fim;
+	
+	public Aresta(Double peso, Vertice incio, Vertice fim) {
+		this.peso = peso;
+		this.inicio = inicio;
+		this.fim = fim;
+	}
+	
+	
+	public Double getPeso() {
+		return peso;
+	}
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+	public Vertice<TIPO> getInicio() {
+		return inicio;
+	}
+	public void setInicio(Vertice<TIPO> inicio) {
+		this.inicio = inicio;
+	}
+	public Vertice<TIPO> getFim() {
+		return fim;
+	}
+	public void setFim(Vertice<TIPO> fim) {
+		this.fim = fim;
+	}
+	
+	
 }
--- Grafo/src/entities/Vertice.java
+++ Grafo/src/entities/Vertice.java
@@ -1,5 +1,35 @@
 package entities;
 
-public class Vertice {
+import java.util.ArrayList;
 
+public class Vertice<TIPO> {
+	private TIPO dado;
+	private ArrayList<Aresta<TIPO>> arestasEntrada;
+	private ArrayList<Aresta<TIPO>> arestasSaida;
+	
+	public Vertice(TIPO valor) {
+		this.dado = dado;
+		this.arestasEntrada = new ArrayList<Aresta<TIPO>>();
+		this.arestasSaida = new ArrayList<Aresta<TIPO>>();
+	}
+
+	public TIPO getDado() {
+		return dado;
+	}
+
+	public void setDado(TIPO dado) {
+		this.dado = dado;
+	}	
+
+	public void adicoinarArestaEntrada(Aresta<TIPO> aresta) {
+		this.arestasEntrada.add(aresta);
+	}
+
+	public void setArestaSaida(Aresta<TIPO> aresta) {
+		this.arestasSaida.add(aresta);
+	}
+	
+	
+	
+	
 }
